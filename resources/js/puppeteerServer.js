10af313c23ec8d5c143019f53c66b32bd562e511
Theodoros Dimitriadis <theoddimi@gmail.com>
2025-02-21 01:33:59 +0200

    prod link

--- resources/js/puppeteerServer.js
+++ resources/js/puppeteerServer.js
@@ -4,7 +4,7 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-console.log("WebSocket server running on ws://localhost:8080");
+console.log("WebSocket server running on wss://cloud.codexdelta.com:8080");
 
 wss.on("connection", (ws) => {
     console.log("Client connected");
